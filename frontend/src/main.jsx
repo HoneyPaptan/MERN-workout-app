8002cff74c211fc3a15441a109fd16d6e9ae02d1
Honey Paptan <hpaptan@gmail.com>
2024-01-30 21:36:34 +0530

    lesson 2 made the whole database for user and routes for it and also the usecontext for the react part

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { WorkoutContextProvider } from './context/WorkoutContext'
+import { AuthContextProvider } from './context/AuthContext'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WorkoutContextProvider>
-      <App />
+    <AuthContextProvider>
+      <WorkoutContextProvider>
+        <App />
 
-    </WorkoutContextProvider>
+      </WorkoutContextProvider>
+
+    </AuthContextProvider>
   </React.StrictMode>,
 )
